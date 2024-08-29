7cbd0ff0df716784966353c4205d3c01af496892
Maria Fernanda <153012436+fernandahervert@users.noreply.github.com>
2024-08-29 00:18:41 -0600

    first commit

---
+++ components/avatar-services.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+import { MotionTransition } from "./transition-component";
+
+const AvatarServices = () => {
+    return (
+        <MotionTransition position='right' className="bottom-0 left-0 hidden md:inline-block md:absolute">
+            <p></p>
+        </MotionTransition>
+    );
+}
+
+export default AvatarServices;
---
+++ data.tsx
@@ -0,0 +1,229 @@
+import { BookText, CodeSquare, HomeIcon, UserRound, Linkedin, Twitter, Rss, Twitch, Youtube, Crop, Pencil, Computer, Book, Rocket, Speech } from "lucide-react";
+
+export const socialNetworks = [
+    {
+        id: 1,
+        logo: <Youtube size={30} strokeWidth={1} />,
+        src: "#!",
+    },
+    {
+        id: 2,
+        logo: <Linkedin size={30} strokeWidth={1} />,
+        src: "#!",
+    },
+    {
+        id: 3,
+        logo: <Twitter size={30} strokeWidth={1} />,
+        src: "#!",
+    },
+    {
+        id: 4,
+        logo: <Rss size={30} strokeWidth={1} />,
+        src: "#!",
+    },
+    {
+        id: 5,
+        logo: <Twitch size={30} strokeWidth={1} />,
+        src: "#!",
+    },
+];
+
+
+export const itemsNavbar = [
+    {
+        id: 1,
+        title: "Home",
+        icon: <HomeIcon size={25} color="#fff" strokeWidth={1} />,
+        link: "/",
+    },
+    {
+        id: 2,
+        title: "User",
+        icon: <UserRound size={25} color="#fff" strokeWidth={1} />,
+        link: "/about-me",
+    },
+    {
+        id: 3,
+        title: "Book",
+        icon: <BookText size={25} color="#fff" strokeWidth={1} />,
+        link: "/services",
+    },
+    {
+        id: 4,
+        title: "Target",
+        icon: <CodeSquare size={25} color="#fff" strokeWidth={1} />,
+        link: "/portfolio",
+    },
+    {
+        id: 5,
+        title: "Home",
+        icon: <Speech size={25} color="#fff" strokeWidth={1} />,
+        link: "/testimonials",
+    },
+];
+
+export const dataAboutPage = [
+    {
+        id: 1,
+        title: "Estudiante de Ingeniería en Software",
+        subtitle: "Universidad Politécnica de Pachuca",
+        description: "Actualmente cursando la carrera de Ingeniería en Software, adquiriendo conocimientos y habilidades clave en el desarrollo de software.",
+        date: "2024",
+    },
+    {
+        id: 2,
+        title: "Frontend Developer",
+        subtitle: "",
+        description: "Desarrollo de interfaces de usuario intuitivas y responsivas utilizando tecnologías modernas de frontend.",
+        date: "2023",
+    },
+    {
+        id: 3,
+        title: "Backend Developer",
+        subtitle: "",
+        description: "Diseño y desarrollo de soluciones de backend escalables y seguras para diversas aplicaciones web.",
+        date: "2022",
+    },
+    {
+        id: 4,
+        title: "Desarrollador Web",
+        subtitle: "",
+        description: "Experiencia en la creación de sitios web completos, desde el frontend hasta el backend, utilizando una variedad de tecnologías y frameworks.",
+        date: "2021",
+    },
+];
+
+export const dataCounter = [
+    {
+        id: 0,
+        endCounter: 10,
+        text: "Años de experiencia",
+        lineRight: true,
+        lineRightMobile: true,
+    },
+    {
+        id: 1,
+        endCounter: 80,
+        text: "Clientes satisfechos",
+        lineRight: true,
+        lineRightMobile: false,
+    },
+    {
+        id: 2,
+        endCounter: 220,
+        text: "Proyectos finalizados",
+        lineRight: true,
+        lineRightMobile: true,
+    },
+    {
+        id: 3,
+        endCounter: 30,
+        text: "Premios ganadores",
+        lineRight: false,
+        lineRightMobile: false,
+    },
+];
+
+export const serviceData = [
+    {
+        icon: <Crop />,
+        title: "Branding",
+        description: "Desarrollo de una identidad de marca sólida y coherente, incluyendo diseño de logotipo, colores y elementos visuales",
+    },
+    {
+        icon: <Pencil />,
+        title: "Diseño web",
+        description: "Diseño creativo y profesional de interfaces web intuitivas y atractivas, centradas en la experiencia del usuario",
+    },
+    {
+        icon: <Computer />,
+        title: "Desarrollo web",
+        description: "Diseño y desarrollo de sitios web a medida, adaptados a tus necesidades",
+    },
+    {
+        icon: <Book />,
+        title: "Copywriting",
+        description: "Creación de contenido persuasivo y atractivo que capta la atención de tu audiencia",
+    },
+    {
+        icon: <Rocket />,
+        title: "SEO",
+        description: "Optimización de tu presencia en línea mediante estrategias de SEO avanzadas, ",
+    },
+];
+
+export const dataPortfolio = [
+    {
+        id: 1,
+        title: "Sneakers App",
+        image: "/image-1.jpg",
+        urlGithub: "#!",
+        urlDemo: "#!",
+    },
+    {
+        id: 2,
+        title: "Game ",
+        image: "/image-2.jpg",
+        urlGithub: "#!",
+        urlDemo: "#!",
+    },
+    {
+        id: 3,
+        title: "Estrategias Web",
+        image: "/image-3.jpg",
+        urlGithub: "#!",
+        urlDemo: "#!",
+    },
+    {
+        id: 4,
+        title: "Vesti App",
+        image: "/image-4.jpg",
+        urlGithub: "#!",
+        urlDemo: "#!",
+    },
+];
+
+export const dataTestimonials = [
+    {
+        id: 1,
+        name: "George Snow",
+        description:
+            "¡Increíble plataforma! Los testimonios aquí son genuinos y me han ayudado a tomar decisiones informadas. ¡Altamente recomendado!",
+        imageUrl: "/profile1.png",
+    },
+    {
+        id: 2,
+        name: "Juan Pérez",
+        description:
+            "Me encanta la variedad de testimonios disponibles en esta página. Es inspirador ver cómo otras personas han superado desafíos similares a los míos. ¡Gracias por esta invaluable fuente de motivación!",
+        imageUrl: "/profile2.png",
+    },
+    {
+        id: 3,
+        name: "María García",
+        description:
+            "Excelente recurso para obtener opiniones auténticas sobre diferentes productos y servicios. Me ha ayudado mucho en mis compras en línea. ¡Bravo por este sitio!",
+        imageUrl: "/profile3.png",
+    },
+    {
+        id: 4,
+        name: "Laura Snow",
+        description:
+            "¡Qué descubrimiento tan fantástico! Los testimonios aquí son honestos y detallados. Me siento más seguro al tomar decisiones después de leer las experiencias compartidas por otros usuarios.",
+        imageUrl: "/profile4.png",
+    },
+    {
+        id: 5,
+        name: "Carlos Sánchez",
+        description:
+            "Una joya en la web. Los testimonios son fáciles de encontrar y están bien organizados. ¡Definitivamente mi destino número uno cuando necesito referencias confiables!",
+        imageUrl: "/profile5.png",
+    },
+    {
+        id: 6,
+        name: "Antonio Martínez",
+        description:
+            "¡Fantástico recurso para aquellos que buscan validación antes de tomar decisiones importantes! Los testimonios aquí son veraces y realmente útiles. ¡Gracias por simplificar mi proceso de toma de decisiones!",
+        imageUrl: "/profile6.png",
+    },
+];
